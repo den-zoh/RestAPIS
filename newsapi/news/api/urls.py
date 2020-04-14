6f5b22230314cf719927bfb6b83f6f8add580ab0
Dennis Mwika <dennis.mwika@strathmore.edu>
2020-04-14 15:02:21 +0300

    class based views

--- newsapi/news/api/urls.py
+++ newsapi/news/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from news.api.views import article_list_create_api_view, article_detail_create_api_view
+# from news.api.views import article_list_create_api_view, article_detail_create_api_view
+from news.api.views import ArticleDetailAPIView, ArticleListCreateView
 
 urlpatterns = [
-    path("articles/", article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>", article_detail_create_api_view, name="article-detail")
+    path("articles/", ArticleListCreateView.as_view(), name="article-list"),
+    path("articles/<int:pk>", ArticleDetailAPIView.as_view(), name="article-detail")
+    
 ]
